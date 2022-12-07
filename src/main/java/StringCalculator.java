981e9ecd7f540c206e78cc87ccdd47eca5bd8416
Ahmed Wael <AhmedWael216@protonmail.com>
2022-12-07 18:10:38 +0200

    ✅ made diffrent delimiters  pass

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -3,8 +3,14 @@ public class StringCalculator {
         if (numbers.length()==0){
             return 0;
         }
-
-        String[] stringsArray = numbers.split(",|\n");
+        String[] stringsArray;
+        if(numbers.charAt(0)=='/' && numbers.charAt(1)=='/'){
+            char del = numbers.charAt(2);
+            numbers=numbers.substring(4);
+            stringsArray = numbers.split(del+"");
+        }else{
+            stringsArray = numbers.split(",|\n");
+        }
         int sum=0;
         for (int i = 0; i < stringsArray.length; i++) {
             sum+=Integer.parseInt(stringsArray[i]);
