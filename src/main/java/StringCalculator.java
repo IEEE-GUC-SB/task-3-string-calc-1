a5efd25f8a87c5cf31cf73fa285822e2c57c782b
Ahmed Wael <AhmedWael216@protonmail.com>
2022-12-07 16:16:42 +0200

    ✅ made zero, single number and simple adding pass

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,5 +1,13 @@
 public class StringCalculator {
     public int add(String numbers){
-    return -1;
+        if (numbers.length()==0){
+            return 0;
+        }
+        String[] stringsArray = numbers.split(",");
+        int sum=0;
+        for (int i = 0; i < stringsArray.length; i++) {
+            sum+=Integer.parseInt(stringsArray[i]);
+        }
+        return sum;
     }
 }
