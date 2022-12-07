6a2e87291e6ac29fde588b3494925f2dfcceaef1
Ahmed Wael <AhmedWael216@protonmail.com>
2022-12-07 16:07:53 +0200

    ❌ failing zero, single number and simple adding

---
+++ src/test/java/stringCalculatorTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class stringCalculatorTest{
+    StringCalculator sc;
+    @BeforeEach
+    public void setUp() {
+        sc =new StringCalculator();
+    }
+    @Test
+    void testZero(){
+        int result = sc.add("");
+        assertEquals(0,result);
+    }
+    @Test
+    void testSingleNumber(){
+        int result = sc.add("1");
+        assertEquals(1,result);
+    }
+    @Test
+    void testSimpleAdd(){
+        int result = sc.add("1,2");
+        assertEquals(3,result);
+    }
+
+
+}
